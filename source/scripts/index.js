c1bda1de801a8deefd5418723c0719929c949b05
CrystalGoose <102854837+CrystalGoose@users.noreply.github.com>
2024-05-31 21:09:43 +0200

    добавляет javascript

---
+++ source/scripts/index.js
@@ -0,0 +1,18 @@
+
+const navigation = document.querySelector('.navigation');
+const navToggle = document.querySelector('.navigation__toggle');
+
+navigation.classList.remove('navigation--nojs');
+navigation.classList.add('navigation--closed');
+
+navToggle.addEventListener('click', () => {
+  if (navigation.classList.contains('navigation--closed')) {
+    navigation.classList.remove('navigation--closed');
+    navigation.classList.add('navigation--opened');
+
+  } else {
+    navigation.classList.add('navigation--closed');
+    navigation.classList.remove('navigation--opened');
+  }
+  console.log('Current classes:', navigation.className);
+});
